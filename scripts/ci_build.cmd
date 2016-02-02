94e6671025432dbea56b88baf46dbc4fbb0532e3
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2016-02-02 10:04:50 -0800

    working on build scripts

--- scripts/ci_build.cmd
+++ scripts/ci_build.cmd
@@ -4,8 +4,9 @@ REM Copyright (c) .NET Foundation and contributors. All rights reserved.
 REM Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 set CI_BUILD=1
-set SKIP_CROSSGEN=0
+set CONFIGURATION=%1
+set VERBOSE=1
 
-CALL %~dp0..\build.cmd %*
+CALL %~dp0..\build.cmd %2
 
 exit /b %errorlevel%
---
+++ scripts/dotnet-cli-build/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.DotNet.Cli.Build.Framework;
+
+namespace Microsoft.DotNet.Cli.Build
+{
+    public class Program
+    {
+        public static void Main(string[] args) => BuildSetup.Create(".NET Core CLI")
+            .UseStandardGoals()
+            .UseAllTargetsFromAssembly<Program>()
+            .Run(args);
+    }
+}
